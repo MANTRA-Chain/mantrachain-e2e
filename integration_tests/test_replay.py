98a97d3608c09fe36f7bc31376665a7e341de1ba
yihuang <yi.codeplayer@gmail.com>
2025-07-14 09:42:00 +0800

    cleanup

--- integration_tests/test_replay.py
+++ integration_tests/test_replay.py
@@ -1,10 +1,13 @@
 from pathlib import Path
-from hexbytes import HexBytes
+
 import pytest
+from eth_contract.create2 import CREATE2_FACTORY
 from eth_utils import to_checksum_address
+from hexbytes import HexBytes
 
 from .network import setup_custom_mantra
-from .utils import send_transaction, KEYS
+from .utils import send_transaction
+
 
 @pytest.fixture(scope="module")
 def mantra_replay(tmp_path_factory):
@@ -16,17 +19,20 @@ def mantra_replay(tmp_path_factory):
 
 def test_replay_tx(mantra_replay):
     w3 = mantra_replay.w3
-    address = to_checksum_address('0x4e59b44847b379578588920ca78fbf26c0b4956c')
-    tx = HexBytes('0xf8a58085174876e800830186a08080b853604580600e600039806000f350fe7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe03601600081602082378035828234f58015156039578182fd5b8082525050506014600cf31ba02222222222222222222222222222222222222222222222222222222222222222a02222222222222222222222222222222222222222222222222222222222222222')
-    signer = to_checksum_address('0x3fab184622dc19b6109349b94811493bf2a45362')
+    tx = HexBytes(
+        "0xf8a58085174876e800830186a08080b853604580600e600039806000f350fe7"
+        "fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+        "e03601600081602082378035828234f58015156039578182fd5b80825250505"
+        "06014600cf31ba0222222222222222222222222222222222222222222222222"
+        "2222222222222222a0222222222222222222222222222222222222222222222"
+        "2222222222222222222"
+    )
+    signer = to_checksum_address("0x3fab184622dc19b6109349b94811493bf2a45362")
 
     fee = 10**17
     if w3.eth.get_balance(signer) < fee:
-        send_transaction(w3, {
-            "to": signer,
-            "value": fee,
-        }, key=KEYS["validator"])
+        send_transaction(w3, {"to": signer, "value": fee})
     txhash = w3.eth.send_raw_transaction(tx)
     receipt = w3.eth.wait_for_transaction_receipt(txhash)
-    assert receipt['status'] == 1
-    assert to_checksum_address(receipt['contractAddress']) == address
+    assert receipt["status"] == 1
+    assert to_checksum_address(receipt["contractAddress"]) == CREATE2_FACTORY
