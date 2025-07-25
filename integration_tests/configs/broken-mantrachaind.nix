b54eca601f1fc1651f7e3f36450d35f75cd8b8b0
mmsqe <tqd0800210105@gmail.com>
2025-07-25 14:37:05 +0800

    Problem: rollback binary depends on default mantrachaind
    use separate one to avoid version mismatch between release and main branch

--- integration_tests/configs/broken-mantrachaind.nix
+++ integration_tests/configs/broken-mantrachaind.nix
@@ -1,5 +1,5 @@
 { pkgs ? import ../../nix { } }:
-let mantrachaind = (pkgs.callPackage ../../nix/mantrachain/. { });
+let mantrachaind = (pkgs.callPackage ../../nix/rollback/. { });
 in
 mantrachaind.overrideAttrs (oldAttrs: {
   patches = oldAttrs.patches or [ ] ++ [
