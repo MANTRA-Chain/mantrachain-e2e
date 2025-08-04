a6306a627da9d69f537b70717f5aace688be8d84
mmsqe <tqd0800210105@gmail.com>
2025-08-04 10:04:45 +0800

    fix ci

--- nix/hermes.nix
+++ nix/hermes.nix
@@ -8,6 +8,8 @@
   openssl,
   pkg-config,
   protobuf,
+  clang,
+  llvmPackages,
 }:
 
 rustPackages_1_83.rustPlatform.buildRustPackage rec {
@@ -17,9 +19,11 @@ rustPackages_1_83.rustPlatform.buildRustPackage rec {
   nativeBuildInputs = [
     pkg-config
     protobuf
+    clang
   ];
   buildInputs = [
     openssl
+    llvmPackages.libclang.lib
   ] ++ lib.optionals stdenv.isDarwin [
     darwin.apple_sdk.frameworks.Security
     darwin.libiconv
@@ -33,6 +37,7 @@ rustPackages_1_83.rustPlatform.buildRustPackage rec {
     RUSTFLAGS = "--cfg ossl111 --cfg ossl110 --cfg ossl101";
     OPENSSL_NO_VENDOR = "1";
     PROTOC = "${protobuf}/bin/protoc";
+    LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
     OPENSSL_DIR = symlinkJoin {
       name = "openssl";
       paths = with openssl; [
