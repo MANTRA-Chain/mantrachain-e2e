bca39674da1956e2e2ba6dae95dbb263e8c6d841
mmsqe <tqd0800210105@gmail.com>
2025-08-26 10:24:27 +0800

    cleanup

--- integration_tests/test_tracers.py
+++ integration_tests/test_tracers.py
@@ -2,8 +2,6 @@ import itertools
 import json
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
-from eth_account import Account
-
 from .expected_constants import (
     EXPECTED_CALLTRACERS,
     EXPECTED_CONTRACT_CREATE_TRACER,
@@ -103,20 +101,18 @@ def test_trace_transactions_tracers(mantra):
         assert tx_res["result"] == EXPECTED_CALLTRACERS, ""
         res = build_contract("TestERC20A")
         contract = w3.eth.contract(abi=res["abi"], bytecode=res["bytecode"])
-        acct = Account.from_key(acc.key)
-        tx = contract.constructor().build_transaction({"from": acct.address})
+        tx = contract.constructor().build_transaction({"from": acc.address})
         tx_hash = send_transaction(w3, tx, key=acc.key)["transactionHash"].hex()
         tx_hash = f"0x{tx_hash}"
         w3_wait_for_new_blocks(w3, 1)
         tx_res = call(method, [tx_hash, tracer])
-        return json.dumps(tx_res["result"], sort_keys=True)
+        assert tx_res["result"] == EXPECTED_CONTRACT_CREATE_TRACER, ""
 
     providers = [mantra.w3]
     with ThreadPoolExecutor(len(providers)) as exec:
         tasks = [exec.submit(process, w3) for w3 in providers]
         res = [future.result() for future in as_completed(tasks)]
         assert len(res) == len(providers)
-        assert res[0] == res[-1] == EXPECTED_CONTRACT_CREATE_TRACER, res
 
 
 def test_trace_tx(mantra):
