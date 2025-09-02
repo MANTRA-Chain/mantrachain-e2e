ef5791ce67ce8d139c4ca9e7e7ee0abe9311a47f
mmsqe <tqd0800210105@gmail.com>
2025-09-02 14:49:32 +0800

    align chain_binary

--- integration_tests/network.py
+++ integration_tests/network.py
@@ -220,7 +220,7 @@ def setup_custom_mantra(
     try:
         if wait_port:
             wait_for_port(ports.rpc_port(base_port))
-        c = Mantra(path / CHAIN_ID, chain_binary=chain_binary or "mantrachaind")
+        c = Mantra(path / CHAIN_ID, chain_binary=chain_binary or chain)
         wait_for_block(c.cosmos_cli(), 1)
         yield c
     finally:
