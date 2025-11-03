210703f0779ca1313a7cdbce7ad100b615b24bb0
mmsqe <tqd0800210105@gmail.com>
2025-11-04 00:28:16 +0800

    Problem: go 1.25 is not supported in nix build

--- nix/evm/default.nix
+++ nix/evm/default.nix
@@ -1,7 +1,8 @@
 {
   lib,
   stdenv,
-  buildGo123Module,
+  buildGoModule,
+  go_1_25,
   fetchFromGitHub,
   rev ? "dirty",
   nativeByteOrder ? true, # nativeByteOrder mode will panic on big endian machines
@@ -13,9 +14,9 @@ let
   pname = "evmd";
 
   # Use static packages for Linux to ensure musl compatibility
-  buildPackages = if stdenv.isLinux then pkgsStatic else { inherit stdenv buildGo123Module; };
+  buildPackages = if stdenv.isLinux then pkgsStatic else { inherit stdenv; buildGoModule = buildGoModule.override { go = go_1_25; }; };
   buildStdenv = buildPackages.stdenv;
-  buildGo123Module' = if stdenv.isLinux then buildPackages.buildGo123Module else buildGo123Module;
+  buildGoModule' = if stdenv.isLinux then buildPackages.buildGoModule else (buildGoModule.override { go = go_1_25; });
 
   tags =
     [
@@ -46,7 +47,7 @@ let
     ];
 
 in
-buildGo123Module' rec {
+  buildGoModule' rec {
   inherit
     pname
     version
@@ -55,21 +56,22 @@ buildGo123Module' rec {
     ;
   stdenv = buildStdenv;
   src = fetchFromGitHub {
-    owner = "MANTRA-Chain";
+    owner = "cosmos";
     repo = "evm";
-    rev = "d8f4ae9d33a6dd51a5698eecb97c21050239d688";
-    hash = "sha256-lhepP1SCK9gE21FL2XqpOuQnmR3axpC6aI3IRrCrqzg=";
+    rev = "f6d50b4dfb19bacc4796fd7e1176cf37d41171f1";
+    hash = "sha256-e6LC7TD9/2m6dt2pyWz2JZQsYJKFverQsleU+3xlvPA=";
   };
   
-  vendorHash = "sha256-IDJHj2e2LBMe0BtwduG7/wLM/C2rRQyIUpbMawJAilk=";
+  vendorHash = "sha256-2IBRbZ6smfRcF8OMmXWEBBXZnvyt3e+3Z3UprSt8ACA=";
   proxyVendor = true;
   sourceRoot = "source/evmd";
   subPackages = [ "cmd/evmd" ];
-  CGO_ENABLED = "1";
+  env.CGO_ENABLED = "1";
 
   preBuild = ''
     mkdir -p $TMPDIR/lib
     export CGO_LDFLAGS="-L$TMPDIR/lib $CGO_LDFLAGS"
+    export GOTOOLCHAIN=local
   '';
 
   doCheck = false;
