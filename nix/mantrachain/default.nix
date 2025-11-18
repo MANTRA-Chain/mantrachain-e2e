70efa9616b9fd33a7c7d0816f151945a1a321b7d
mmsqe <tqd0800210105@gmail.com>
2025-11-19 00:23:02 +0800

    rename

--- nix/mantrachain/default.nix
+++ nix/mantrachain/default.nix
@@ -14,7 +14,7 @@ in
 builder {
   version = "v7.0.0-rc1";
   owner = "mmsqe";
-  rev = "822f10b53ae29f338f8bcb92f05fd7408d16466b"; 
-  hash = "sha256-/M1h15MZYoLj5tzQqDHgymq+/L1I6EIsuDRkgdMfIUg=";
-  vendorHash = "sha256-Q+52fqkBTdBn64KC2vSwbkFkjc9dZ9HKr0oDqxyQZIw=";
+  rev = "3ad5f46686bb885cb6a3907eb3a064ff00bda6dc"; 
+  hash = "sha256-Tv/XAwZ4ebdKEcy72bKAyW0Pz7tLj+9HINHIiqag7/s=";
+  vendorHash = "sha256-kbM/j1GFbI/1hLpWDBbGwnBQc7B0TjwVv8dPrLSaKAM=";
 }
