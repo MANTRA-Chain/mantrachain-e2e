447e7c06fb7042a667e8b2a98b2fda15defd2456
mmsqe <tqd0800210105@gmail.com>
2025-11-21 15:54:07 +0800

    async

--- integration_tests/test_upgrade_recent.py
+++ integration_tests/test_upgrade_recent.py
@@ -14,15 +14,16 @@ from .upgrade_utils import (
     setup_mantra_upgrade,
 )
 from .utils import (
+    ACCOUNTS,
     ADDRS,
     CHAIN_ID,
     SCALE_FACTOR,
-    Greeter,
-    call_with_retry,
+    AsyncGreeter,
+    call_with_retry_async,
     update_node_cmd,
 )
 
-pytestmark = [pytest.mark.slow, pytest.mark.skipped]
+pytestmark = [pytest.mark.asyncio, pytest.mark.skipped]
 
 
 @pytest.fixture(scope="module")
@@ -38,15 +39,20 @@ def custom_mantra(request, tmp_path_factory):
     )
 
 
-def exec(c):
+async def exec(c):
     cli = c.cosmos_cli()
-    w3 = c.w3
+    w3 = c.async_w3
+    deployer = ACCOUNTS["community"]
     community = ADDRS["community"]
-    greeter = Greeter("Greeter")
-    greeter.deploy(w3)
-    old_height = cli.block_height()
+    greeter = AsyncGreeter()
+    await greeter.deploy(w3)
 
-    balance_bf = w3.eth.get_balance(community, block_identifier=old_height)
+    old_height = cli.block_height()
+    print("mm-old_height", old_height)
+    res = await greeter.greet(block_identifier=old_height)
+    print("mm-res", res)
+
+    balance_bf = await w3.eth.get_balance(community, block_identifier=old_height)
 
     wait_for_new_blocks(cli, 1)
     grpc_cmd = cli.raw.cmd
@@ -54,10 +60,12 @@ def exec(c):
     update_node_cmd(c.base_dir, grpc_cmd, 1, grpc_only=True)
 
     target_height0 = cli.block_height() + 15
+    print("mm-target_height0", target_height0)
     cli = do_upgrade(c, "v7.0.0-rc0", target_height0, denom=LEGACY_DENOM)
 
-    target_height = cli.block_height() + 15
-    cli = do_upgrade(c, "v7.0.0-rc1", target_height, min_deposit=1 * SCALE_FACTOR)
+    target_height1 = cli.block_height() + 15
+    print("mm-target_height1", target_height1)
+    cli = do_upgrade(c, "v7.0.0-rc1", target_height1, min_deposit=1 * SCALE_FACTOR)
 
     grpc_node = 1
     api_port = ports.api_port(c.base_port(grpc_node))
@@ -70,16 +78,18 @@ def exec(c):
             stderr=subprocess.STDOUT,
         )
 
-    def test_historical_queries():
-        tx = greeter.contract.functions.setGreeting("world").build_transaction(
-            {"from": community}
+    async def test_historical_queries():
+        data = greeter.contract.fns.setGreeting("world").data
+        tx = {"to": greeter.address, "from": community, "data": data}
+        assert await greeter.greet(block_identifier=old_height) == "Hello"
+        assert await w3.eth.estimate_gas(tx, block_identifier=old_height) > 0
+        assert (
+            await w3.eth.get_balance(community, block_identifier=old_height)
+            == balance_bf
         )
-        assert greeter.contract.caller(block_identifier=old_height).greet() == "Hello"
-        assert w3.eth.estimate_gas(tx, block_identifier=old_height) > 0
-        assert w3.eth.get_balance(community, block_identifier=old_height) == balance_bf
 
-    def query_greeter():
-        return greeter.contract.caller(block_identifier=old_height).greet()
+    async def query_greeter():
+        return await greeter.greet(block_identifier=old_height)
 
     # run grpc-only mode directly with existing chain state
     with (c.base_dir / "node1.log").open("a") as logfile:
@@ -109,24 +119,24 @@ def exec(c):
             proc.terminate()
             proc.wait(timeout=5)
 
-            assert call_with_retry(query_greeter, expect_error=True)
+            assert await call_with_retry_async(query_greeter, expect_error=True)
 
-            balance = w3.eth.get_balance(community)
+            balance = await w3.eth.get_balance(community)
             proc = start_grpc_node(logfile)
             for port in (grpc_port, api_port):
                 wait_for_port(port)
             wait_for_new_blocks(cli, 1)
 
-            assert call_with_retry(query_greeter, expect_error=False)
+            assert await call_with_retry_async(query_greeter, expect_error=False)
             # test historical queries work after reconnection
-            test_historical_queries()
-            assert w3.eth.get_balance(community) == balance
+            await test_historical_queries()
+            assert await w3.eth.get_balance(community) == balance
         finally:
             if proc.poll() is None:
                 proc.terminate()
                 proc.wait()
 
 
-def test_cosmovisor_upgrade(custom_mantra: Mantra):
-    exec(custom_mantra)
+async def test_cosmovisor_upgrade(custom_mantra: Mantra):
+    await exec(custom_mantra)
     cleanup_upgrades_folder(custom_mantra.cosmos_cli().data_dir)
