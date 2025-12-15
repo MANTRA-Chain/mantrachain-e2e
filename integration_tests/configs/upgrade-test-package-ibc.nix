3f32822b0b05bbe45f64fe7f887342cec0fd132d
mmsqe <tqd0800210105@gmail.com>
2025-12-15 15:43:42 +0800

    bump deps

--- integration_tests/configs/upgrade-test-package-ibc.nix
+++ integration_tests/configs/upgrade-test-package-ibc.nix
@@ -6,7 +6,7 @@ let
   common = import ./mantrachain-common.nix { inherit pkgs; };
   platform = common.platform;
   releases = {
-    genesis = pkgs.callPackage ../../nix/v6.1.1/default.nix {};
+    genesis = pkgs.callPackage ../../nix/v6.1.3/default.nix {};
     "v7.0.0-rc4" = if useLiteMode
       then common.localMantrachaindWrapper
       else pkgs.mantrachaind;
