c2684200a1306f3232216106a92c4f0b8e98ae17
mmsqe <tqd0800210105@gmail.com>
2025-12-31 18:22:14 +0800

    Problem: selfhost runner is not stable

--- integration_tests/utils.py
+++ integration_tests/utils.py
@@ -811,7 +811,7 @@ def approve_proposal(n, events, event_query_tx=True, **kwargs):
             **kwargs,
         )
         assert rsp["code"] == 0, rsp["raw_log"]
-    wait_for_new_blocks(cli, 1, sleep=0.01)
+    wait_for_new_blocks(cli, 1)
     res = cli.query_tally(proposal_id)
     res = res.get("tally") or res
     assert (
