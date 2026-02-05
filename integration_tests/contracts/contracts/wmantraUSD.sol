1f7cf2ee84af4c2a4601a12d09cb417b05581de3
freeelancer <lance.lan@mantra.finance>
2026-02-05 12:01:45 +0800

    feat: align wmantraUSD to standard erc20wrapper

--- integration_tests/contracts/contracts/wmantraUSD.sol
+++ integration_tests/contracts/contracts/wmantraUSD.sol
@@ -2,61 +2,92 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Wrapper.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-contract wmantraUSD is ERC20 {
-    using SafeERC20 for IERC20;
+contract wmantraUSD is ERC20, ERC20Wrapper {
+    uint256 private constant SCALAR = 10 ** 12; // 10^18 / 10^6
 
-    IERC20 public immutable mantraUSD;
-    uint256 private constant SCALAR = 10**12; // 10^18 / 10^6
+    event Deposit(
+        address sender,
+        address indexed account,
+        uint256 depositedValue,
+        uint256 mintedValue
+    );
+    event Withdrawal(
+        address sender,
+        address indexed account,
+        uint256 withdrawValue,
+        uint256 burnedValue
+    );
 
-    constructor(address _mantrausdAddress) ERC20("Wrapped mantraUSDC", "wmantraUSD") {
-        require(_mantrausdAddress != address(0), "Invalid address");
-        mantraUSD = IERC20(_mantrausdAddress);
+    constructor(
+        IERC20 underlyingToken
+    ) ERC20("Wrapped mantraUSD", "wmantraUSD") ERC20Wrapper(underlyingToken) {}
+
+    function decimals()
+        public
+        view
+        virtual
+        override(ERC20, ERC20Wrapper)
+        returns (uint8)
+    {
+        return 18;
     }
 
-    // Note: bridging (x/erc20 convert + ICS20 transfer) is handled by the frontend
-    // calling the relevant precompiles directly.
-
     /**
-     * @dev Deposit mantraUSD (6 decimals) -> Mint wmantraUSD (18 decimals)
+     * @dev Allow a user to deposit underlying tokens and mint the corresponding number of wrapped tokens.
      */
-    function deposit(uint256 amountMantraUSD) external {
-        require(amountMantraUSD > 0, "Amount must be greater than 0");
-
-        mantraUSD.safeTransferFrom(msg.sender, address(this), amountMantraUSD);
-        
+    function depositFor(
+        address account,
+        uint256 value
+    ) public virtual override returns (bool) {
+        address sender = _msgSender();
+        if (sender == address(this)) {
+            revert ERC20InvalidSender(address(this));
+        }
+        if (account == address(this)) {
+            revert ERC20InvalidReceiver(account);
+        }
+        SafeERC20.safeTransferFrom(
+            this.underlying(),
+            sender,
+            address(this),
+            value
+        );
         // Scale up
-        uint256 amountToMint = amountMantraUSD * SCALAR;
-        _mint(msg.sender, amountToMint);
-        
-        emit Deposit(msg.sender, amountMantraUSD);
+        uint256 amountToMint = value * SCALAR;
+        _mint(account, amountToMint);
+        emit Deposit(sender, account, value, amountToMint);
+        return true;
     }
 
     /**
-     * @dev Withdraw wmantraUSD -> mantraUSD. 
-     * If the user inputs an amount with dust (e.g., 1.000000000001), 
-     * we only burn the clean 1.0 part and leave the dust in their wallet.
+     * @dev Allow a user to burn a number of wrapped tokens and withdraw the corresponding number of underlying tokens.
      */
-    function withdraw(uint256 amountWmantraUSD) external {
-        require(balanceOf(msg.sender) >= amountWmantraUSD, "Insufficient balance");
+    function withdrawTo(
+        address account,
+        uint256 value
+    ) public virtual override returns (bool) {
+        if (account == address(this)) {
+            revert ERC20InvalidReceiver(account);
+        }
+        address sender = _msgSender();
+
+        require(balanceOf(sender) >= value, "Insufficient balance");
 
         // 1. Calculate the clean mantraUSD amount (Integer division handles the truncation)
-        uint256 mantraUSDToReturn = amountWmantraUSD / SCALAR;
+        uint256 mantraUSDToReturn = value / SCALAR;
         require(mantraUSDToReturn > 0, "Amount too small to withdraw");
 
         // 2. Calculate the exact wmantraUSD amount required for that mantraUSD
         // Any remainder from the input `amountWmantraUSD` is ignored and not burned.
         uint256 wmantraUSDToBurn = mantraUSDToReturn * SCALAR;
+        _burn(sender, wmantraUSDToBurn);
 
-        // 3. Burn only the convertable part
-        _burn(msg.sender, wmantraUSDToBurn);
-        // 4. Send the mantraUSD
-        mantraUSD.safeTransfer(msg.sender, mantraUSDToReturn);
-
-        emit Withdrawal(msg.sender, mantraUSDToReturn);
+        // 3. Send the underlying token
+        SafeERC20.safeTransfer(this.underlying(), account, mantraUSDToReturn);
+        emit Withdrawal(sender, account, mantraUSDToReturn, wmantraUSDToBurn);
+        return true;
     }
-
-    event Deposit(address indexed user, uint256 mantraUSDAmount);
-    event Withdrawal(address indexed user, uint256 mantraUSDAmount);
 }
