dcb1a634b1c9291c01a200d609adfc265d06adf8
mmsqe <tqd0800210105@gmail.com>
2026-02-11 09:20:19 +0800

    Problem: revoke grants of blacklisted user is not covered

--- nix/mantrachain/default.nix
+++ nix/mantrachain/default.nix
@@ -14,7 +14,7 @@ in
 builder {
   version = "v8";
   owner = "MANTRA-Chain";
-  rev = "a7e7e0736e894f4c8ff29097044c5fa0ed2057ec";
-  hash = "sha256-sSVzuVvbI6JnmIubHRTXY6aUi6yXu/5rfou+JYmX1fU=";
+  rev = "5aa769b59d1bfbc6199cf735e71cdf5cd5e74def";
+  hash = "sha256-J+/QYtIlKug8mOf4SzsWIO7j5A28Nak/Kcr8ITvy7Q0=";
   vendorHash = "sha256-eL/vouCkuZsTOXD7OLMeaJJvV0Zk5N4mWz/CGWc9KDo=";
 }
