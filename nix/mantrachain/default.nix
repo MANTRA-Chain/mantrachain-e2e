6f766374f51f78c8765c9d99fa4d3ad1a432b818
mmsqe <tqd0800210105@gmail.com>
2026-02-12 10:01:11 +0800

    Problem: no test for revoke feegrant allowances when blacklist is granter

--- nix/mantrachain/default.nix
+++ nix/mantrachain/default.nix
@@ -14,7 +14,7 @@ in
 builder {
   version = "v8";
   owner = "MANTRA-Chain";
-  rev = "15578bcae66377dd48167214afb665c45e7f6116";
-  hash = "sha256-WW0vD/pJ/80BP4VFpF3BpPjIOJXU4ZJOcXpeSOx44I8=";
+  rev = "701771a276f51576c916d9b18ba7ee6eb73995ad";
+  hash = "sha256-wntTBocswbo/Lut+/FTn+y50pAeSJJdyV1H3CJuglwI=";
   vendorHash = "sha256-h26r91LLIqy/xz+sFuvpgNMKMCLcNvQJs53Jmri7dM4=";
 }
