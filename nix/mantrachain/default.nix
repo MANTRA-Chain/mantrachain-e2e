0286ce703d2bb3a6f622fb4c98b78fd99d126781
mmsqe <tqd0800210105@gmail.com>
2026-04-10 16:48:23 +0800

    Problem: fix-blacklist-revoke is not tested

--- nix/mantrachain/default.nix
+++ nix/mantrachain/default.nix
@@ -14,7 +14,7 @@ in
 builder {
   version = "v8";
   owner = "MANTRA-Chain";
-  rev = "49312507982481350059dd48162a1755dce7428e";
-  hash = "sha256-8a6FkmKJifSgLxMNRKuSk80wL8qCtqziudRnwmRNgjU=";
-  vendorHash = "sha256-RmkrtiN1bElnFzwjpnuhZAvbTsJG0p3MrkVZ7LqcMsY=";
+  rev = "f0ad92aa60e5adc8c545a943b5c1b2cacfa8f3aa";
+  hash = "sha256-sO/m43x+CwS/wiQAE7336pEsT4p6vyvS5tIkOoDDIj4=";
+  vendorHash = "sha256-zL0GxEZKQIVF0lPfydG+lhB/ECKKo1hSfrnilTIuHL0=";
 }
