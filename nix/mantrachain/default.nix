7c9de971179a643f1490cbc4bb56947a91fa22ad
mmsqe <tqd0800210105@gmail.com>
2026-05-05 17:20:58 +0800

    Problem: default coininfo via flag is not tested

--- nix/mantrachain/default.nix
+++ nix/mantrachain/default.nix
@@ -14,7 +14,7 @@ in
 builder {
   version = "v8.1.1";
   owner = "MANTRA-Chain";
-  rev = "eb92b6bac5f8e7a2406cc21de6b24941e9b6ddb1";
-  hash = "sha256-v0w9bCunOWahAAU5jiwLDRJwpBSsC7Y2VQEnaqX24ZU=";
+  rev = "8aba8560d2e21fe2cb8394472178e093275051e7";
+  hash = "sha256-GxCv5g8H3xRUTNF7o6rDI05B3+pPw0JokdrPrRxCvK8=";
   vendorHash = "sha256-Yn8c7H4Ihg6Wr87Xt4geyn+RSceg4t1+2ba9+iFYq18=";
 }
